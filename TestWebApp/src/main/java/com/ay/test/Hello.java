1d177c1da2f82d11aa0bc74347a3f4f3865ede1f
Anurag Yadav <anurag@Anurags-MacBook-Pro.local>
2017-02-17 12:55:58 +0530

    First commit

---
+++ TestWebApp/src/main/java/com/ay/test/Hello.java
@@ -0,0 +1,14 @@
+package com.ay.test;
+
+public class Hello {
+
+
+    public static String sayHello() {
+	return "Hello World!!!";
+    }
+    
+    public static void main (String args[]) {
+
+    }
+
+}
